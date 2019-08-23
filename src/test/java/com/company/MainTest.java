6e9c4fcf1b93275add4e2eeb99361f750fab07e6
arturperec <artur.g.perec@gmail.com>
2019-08-23 19:17:40 +0200

    added MainTest template

--- src/test/java/com/company/MainTest.java
+++ src/test/java/com/company/MainTest.java
@@ -1,5 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.company;
 
 class MainTest {
 
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        System.out.println("Setting up!");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
 }
