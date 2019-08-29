bdbf1449f496132c3573ecf67508a7e7eab9d66f
arturperec <artur.g.perec@gmail.com>
2019-08-29 17:32:06 +0200

    Clearance for TDD

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,11 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Human testHuman = new Human("Jan", "Nowak", "90120512335");
-        testHuman.printBasicData();
-
-        Patient testPatient = new Patient("Robert", "Makowski", "94101065415", true);
-        testPatient.printBasicData();
-        testPatient.printInsuranceStatus();
     }
 }
