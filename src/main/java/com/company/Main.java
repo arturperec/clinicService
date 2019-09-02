aa2893618d2115cc536b398ca0f3cf0a875ddb85
arturperec <artur.g.perec@gmail.com>
2019-09-02 13:25:50 +0200

    updated test classes calls

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         Human human = new Human();
-        human.setHumanData("Jarosław", "Bąkosław", "93021030290");
-        System.out.println(human.getHumanData());
+        human.setBasicData("Jarosław", "Bąkosław", "93021030290");
+        System.out.println(human.getBasicData());
+
+        System.out.print("\n");
+
+        Patient testPatient = new Patient();
+        testPatient.setBasicData("Patryk", "Pacjenik", "93022039412", true);
+        System.out.println(testPatient.getBasicData());
+
     }
 }
