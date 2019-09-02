bcccb608d31f9d668684fc23bffc88911f06fc55
arturperec <artur.g.perec@gmail.com>
2019-09-02 10:58:32 +0200

    added test class calls for Main class

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Human human = new Human();
+        human.setHumanData("Jarosław", "Bąkosław", "93021030290");
+        System.out.println(human.getHumanData());
     }
 }
